585940f8eb760b9867453db1b839ecd52151c84f
Andreas Stange <anst.dev@gmail.com>
2021-03-17 20:19:12 +0100

    download MLAPI and added Remote Procedure Call example in MainScene
    -> when running, in NetworkingManager click "Start host" in inspector

---
+++ UltraStar Play/Assets/Common/Network/NetworkRpcDemo.cs
@@ -0,0 +1,41 @@
+﻿using MLAPI;
+using MLAPI.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkRpcDemo : NetworkedBehaviour
+{
+    public override void NetworkStart()
+    {
+        Debug.Log("NetworkStart");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("SendRandomInt", GUILayout.Width(600), GUILayout.Height(300)))
+        {
+            if (IsServer)
+            {
+                InvokeClientRpcOnEveryone(MyClientRPC, Random.Range(-50, 50));
+            }
+            else
+            {
+                InvokeServerRpc(MyServerRPC, Random.Range(-50, 50));
+            }
+        }
+    }
+
+    [ServerRPC]
+    private void MyServerRPC(int number)
+    {
+        Debug.Log("The number received was: " + number);
+        Debug.Log("This method ran on the server upon the request of a client");
+    }
+
+    [ClientRPC]
+    private void MyClientRPC(int number)
+    {
+        Debug.Log("The number received was: " + number);
+        Debug.Log("This method ran on the client upon the request of the server");
+    }
+}
